d09e9457e8ed64cf9e7fc9872d85daca9d557487
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-17 10:03:40 +0530

    roboshopansiblecode

--- roboshop-infra/main.tf
+++ roboshop-infra/main.tf
@@ -6,17 +6,3 @@ module "ec2" {
     instance_type = each.value["type"]
   
 }
-
-module "sg" {
-    for_each = var.instance
-    source = "./sg"
-}
-
-
-module "route53" {
-    for_each = var.instance
-    source = "./route53"
-    component = each.value["name"]
-  
-}
-
