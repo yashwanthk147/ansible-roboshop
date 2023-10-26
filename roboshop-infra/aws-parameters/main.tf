acd0555c1cfc488a332e43a881ace2e6ace2f804
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-26 15:42:39 +0530

    roboshopansiblecode

--- roboshop-infra/aws-parameters/main.tf
+++ roboshop-infra/aws-parameters/main.tf
@@ -1,8 +1,3 @@
-terraform {
-    backend "s3" {}
-}
-
-
 resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
---
+++ roboshop-infra/env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "roboshop-s3"
+key = "dev/infra/terraform.tfstate"
+region = "us-east-1"
