fd6d20136f0c87c57476c9b3259632f5b1d5bf26
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-26 15:43:17 +0530

    roboshopansiblecode

--- roboshop-infra/main.tf
+++ roboshop-infra/main.tf
@@ -1,6 +1,6 @@
 #iterate on modules rather than individual resources
 module "ec2" {
-    for_each = var.instance
+    for_each = var.instances
     source = "./ec2"
     component = each.value["name"]
     instance_type = each.value["type"]
